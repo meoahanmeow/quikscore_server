9c6e73f169ff2b88879207f9c20f4a17986acd8c
meoahanmeow <neopromdon@gmail.com>
2025-09-23 21:25:27 +0700

    f

--- api/login.js
+++ api/login.js
@@ -5,22 +5,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Read env vars
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
-// POST /api/login
 app.post((req, res) => {
   const { username, password } = req.body;
-
   if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
     return res.status(200).json({ success: true, message: "SUCCESS!" });
   }
-
-  res.status(400).json({
-    success: false,
-    message: "Invalid Credentials, please contact admin",
-  });
+  res.status(400).json({ success: false, message: "Invalid Credentials" });
 });
 
 export default app;
