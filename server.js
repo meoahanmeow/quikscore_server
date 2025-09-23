166d7185ca9862219c43a325b7cd91794042253e
meoahanmeow <neopromdon@gmail.com>
2025-09-23 21:08:05 +0700

    fix: api end points

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/login", (req, res) => {
+app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
 
   if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
