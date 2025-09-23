0b1fce7956762e58c1a5e34aa4e5b90f0c3e8372
meoahanmeow <neopromdon@gmail.com>
2025-09-23 21:39:47 +0700

    fix: vercel

--- api/login.js
+++ api/login.js
@@ -8,11 +8,13 @@ app.use(cors());
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
-app.post((req, res) => {
+app.post("/", (req, res) => {
   const { username, password } = req.body;
+
   if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
     return res.status(200).json({ success: true, message: "SUCCESS!" });
   }
+
   res.status(400).json({ success: false, message: "Invalid Credentials" });
 });
 
