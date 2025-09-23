e04dcf0622b11cd8b5df2d064e4e3ab2604e808a
meoahanmeow <neopromdon@gmail.com>
2025-09-23 20:59:17 +0700

    fix: for vercel

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config();
 
-const PORT = 5000;
+dotenv.config();
 
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
@@ -13,15 +12,15 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/login", (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    if(username == ADMIN_USERNAME && password == ADMIN_PASSWORD) {
-        res.status(200).json({success: true, message: "SUCCESS!"});
-        return;
-    }
-    res.status(400).json({success: false, message: "Invalid Credentials, please contact admin"});
-})
+  if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
+    return res.status(200).json({ success: true, message: "SUCCESS!" });
+  }
 
-app.listen(PORT, () => {
-    console.log(`Server is currently listening to port ${PORT}`);
-})
+  res
+    .status(400)
+    .json({ success: false, message: "Invalid Credentials, please contact admin" });
+});
+
+export default app;
