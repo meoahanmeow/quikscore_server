df033c1b458ef6255c87160aadb5f8a84f6469d7
meoahanmeow <neopromdon@gmail.com>
2025-09-23 21:18:35 +0700

    fix: vercel

--- api/login.js
+++ api/login.js
@@ -1,26 +1,26 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
-const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/api/login", (req, res) => {
+// Read env vars
+const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+
+// POST /api/login
+app.post((req, res) => {
   const { username, password } = req.body;
 
   if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
     return res.status(200).json({ success: true, message: "SUCCESS!" });
   }
 
-  res
-    .status(400)
-    .json({ success: false, message: "Invalid Credentials, please contact admin" });
+  res.status(400).json({
+    success: false,
+    message: "Invalid Credentials, please contact admin",
+  });
 });
 
 export default app;
